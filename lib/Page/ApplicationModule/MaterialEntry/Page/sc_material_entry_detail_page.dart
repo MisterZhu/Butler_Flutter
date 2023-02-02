b3072d0f812df3f2b70df49d6ea7b5ce6183566a
mjl <2994355324@qq.com>
2023-02-02 15:57:42 +0800

    代码优化

---
+++ lib/Page/ApplicationModule/MaterialEntry/Page/sc_material_entry_detail_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:smartcommunity/Skin/View/sc_custom_scaffold.dart';
+
+/// 入库详情
+
+class SCMaterialEntryDetailPage extends StatefulWidget {
+  @override
+  SCMaterialEntryDetailPageState createState() => SCMaterialEntryDetailPageState();
+}
+
+class SCMaterialEntryDetailPageState extends State<SCMaterialEntryDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SCCustomScaffold(body: body());
+  }
+
+  /// body
+  body() {
+    return Container();
+  }
+}
