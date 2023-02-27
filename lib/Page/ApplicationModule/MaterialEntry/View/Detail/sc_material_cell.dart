825facc20629931246e7e2981e609885810cef99
mjl <2994355324@qq.com>
2023-02-27 14:45:02 +0800

    盘点优化

--- lib/Page/ApplicationModule/MaterialEntry/View/Detail/sc_material_cell.dart
+++ lib/Page/ApplicationModule/MaterialEntry/View/Detail/sc_material_cell.dart
@@ -15,10 +15,21 @@ const int scMaterialCellTypeRadio = 1;
 /// 删除cell
 const int scMaterialCellTypeDelete = 2;
 
+/// 盘点cell
+const int scMaterialCellTypeInventory = 3;
+
 /// 物资cell
 class SCMaterialCell extends StatefulWidget {
-
-  const SCMaterialCell({Key? key, required this.type, this.model, this.onTap, this.numChangeAction, this.radioTap, this.deleteAction, this.hideMaterialNumTextField}) : super(key: key);
+  const SCMaterialCell(
+      {Key? key,
+      required this.type,
+      this.model,
+      this.onTap,
+      this.numChangeAction,
+      this.radioTap,
+      this.deleteAction,
+      this.hideMaterialNumTextField})
+      : super(key: key);
 
   final SCMaterialListModel? model;
 
@@ -69,6 +80,8 @@ class SCMaterialCellState extends State<SCMaterialCell> {
       return radioCell();
     } else if (widget.type == scMaterialCellTypeDelete) {
       return deleteCell();
+    } else if (widget.type == scMaterialCellTypeInventory) {
+      return inventoryCell();
     } else {
       return normalCell();
     }
@@ -147,26 +160,116 @@ class SCMaterialCellState extends State<SCMaterialCell> {
     );
   }
 
+  /// 盘点cell
+  Widget inventoryCell() {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          inventoryImageView(),
+          const SizedBox(
+            width: 8.0,
+          ),
+          detailInfoView(),
+          const SizedBox(
+            width: 10.0,
+          ),
+          numView()
+        ],
+      ),
+    );
+  }
+
   /// 物资图片
   Widget imageView() {
     String url = SCConfig.getImageUrl(widget.model?.pic ?? '');
     return Container(
+        width: 80.0,
+        height: 80.0,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(4.0),
+            color: SCColors.color_D9D9D9),
+        child: SCImage(
+          url: url,
+          width: 80.0,
+          height: 80.0,
+          fit: BoxFit.cover,
+        ));
+  }
+
+  /// 盘点的物资图片
+  Widget inventoryImageView() {
+    String url = SCConfig.getImageUrl(widget.model?.pic ?? '');
+    return Container(
+        width: 80.0,
+        height: 80.0,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(4.0),
+            color: SCColors.color_D9D9D9),
+        child: Stack(
+          children: [
+            SCImage(
+              url: url,
+              width: 80.0,
+              height: 80.0,
+              fit: BoxFit.cover,
+            ),
+            Positioned(
+                left: 0,
+                right: 0,
+                bottom: 0,
+                child: inventoryNumberView())
+          ],
+        ));
+  }
+
+  /// 盘点数量view
+  Widget inventoryNumberView() {
+    int number = widget.model?.number ?? 0; // 账存
+    int checkNum = widget.model?.checkNum ?? 0; // 盘点数量
+    int? resultNum = widget.model?.resultNum ?? 0; // 盘盈或盘平数量
+    Color bgColor = SCColors.color_F7F8FA;// 背景颜色
+    Color textColor = SCColors.color_5E5F66;// 文字颜色
+    String text = '';
+    if (number < checkNum) {// 盘赢
+      bgColor = SCColors.color_E3FFF1.withOpacity(0.9);
+      textColor = SCColors.color_00B42A;
+      text = "盘赢$resultNum";
+    } else if (number > checkNum) {// 盘亏
+      bgColor = SCColors.color_FFF1F0;
+      textColor = SCColors.color_FF4040;
+      text = "盘亏$resultNum";
+    } else {// 盘平
+      bgColor = SCColors.color_F7F8FA;
+      textColor = SCColors.color_5E5F66;
+      text = "盘平$resultNum";
+    }
+    return Container(
       width: 80.0,
-      height: 80.0,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(4.0),
-          color: SCColors.color_D9D9D9),
-      child: SCImage(url: url, width: 80.0, height: 80.0, fit: BoxFit.cover,)
+      height: 20.0,
+      alignment: Alignment.center,
+      color: bgColor,
+      child: Text(
+        text,
+        textAlign: TextAlign.center,
+        maxLines: 1,
+        overflow: TextOverflow.ellipsis,
+        style: TextStyle(
+            fontSize: SCFonts.f12,
+            fontWeight: FontWeight.w400,
+            color: textColor),
+      ),
     );
   }
 
   /// 物资信息-详情
   Widget detailInfoView() {
     return Expanded(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: [nameLabel(), infoLabel(10)],
+        child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [nameLabel(), infoLabel(10)],
     ));
   }
 
@@ -188,59 +291,63 @@ class SCMaterialCellState extends State<SCMaterialCell> {
     }
     return Expanded(
         child: SizedBox(
-          height: 80.0,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
+      height: 80.0,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          nameLabel(),
+          Expanded(
+              child: Row(
             children: [
-              nameLabel(),
-              Expanded(
-                  child: Row(
-                    children: [
-                      Expanded(child: infoLabel(2)),
-                      Column(
-                        children: [
-                          Expanded(child: Container(color: Colors.transparent),),
-                          stepper
-                        ],
-                      )
-                    ],
-                  ))
+              Expanded(child: infoLabel(2)),
+              Column(
+                children: [
+                  Expanded(
+                    child: Container(color: Colors.transparent),
+                  ),
+                  stepper
+                ],
+              )
             ],
-          ),
-        ));
+          ))
+        ],
+      ),
+    ));
   }
 
   /// 物资信息-删除
   Widget deleteInfoView() {
     return Expanded(
         child: SizedBox(
-          height: 80.0,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
+      height: 80.0,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          nameLabel(),
+          Expanded(
+              child: Row(
             children: [
-              nameLabel(),
-              Expanded(
-                  child: Row(
-                    children: [
-                      Expanded(child: infoLabel(2)),
-                      Column(
-                        children: [
-                          Expanded(child: Container(),),
-                          SCStepper(
-                            num: widget.model?.localNum,
-                            numChangeAction: (int value) {
-                              widget.numChangeAction?.call(value);
-                            },
-                          )
-                        ],
-                      )
-                    ],
-                  ))
+              Expanded(child: infoLabel(2)),
+              Column(
+                children: [
+                  Expanded(
+                    child: Container(),
+                  ),
+                  SCStepper(
+                    num: widget.model?.localNum,
+                    numChangeAction: (int value) {
+                      widget.numChangeAction?.call(value);
+                    },
+                  )
+                ],
+              )
             ],
-          ),
-        ));
+          ))
+        ],
+      ),
+    ));
   }
 
   /// 物资名称label
@@ -265,8 +372,16 @@ class SCMaterialCellState extends State<SCMaterialCell> {
 
   /// 物资信息label
   Widget infoLabel(int maxLength) {
+    String text = '';
+    if (widget.type == scMaterialCellTypeInventory) {
+      text =
+          '单位:${widget.model?.unitName} 条形码:${widget.model?.barCode}\n规格:${widget.model?.norms}\n账面库存:${widget.model?.number}';
+    } else {
+      text =
+          '单位:${widget.model?.unitName} 条形码:${widget.model?.barCode}\n规格:${widget.model?.norms}';
+    }
     return Text(
-      '单位:${widget.model?.unitName} 条形码:${widget.model?.barCode}\n规格:${widget.model?.norms}',
+      text,
       maxLines: maxLength,
       overflow: TextOverflow.ellipsis,
       style: const TextStyle(
@@ -278,10 +393,18 @@ class SCMaterialCellState extends State<SCMaterialCell> {
 
   /// 物资数量
   Widget numView() {
+    String text = '';
+    if (widget.type == scMaterialCellTypeInventory) {// 盘点
+      int checkNum = widget.model?.checkNum ?? 0;
+      text = 'x$checkNum';
+    } else {// 其他
+      int num = widget.model?.number ?? 0;
+      text = 'x$num';
+    }
     return Padding(
       padding: const EdgeInsets.only(top: 22.0),
       child: Text(
-        'x${widget.model?.number}',
+        text,
         style: const TextStyle(
             fontSize: SCFonts.f14,
             fontWeight: FontWeight.w400,
