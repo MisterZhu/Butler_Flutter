252c64a7857840b375a0c1410ddee71de8cfa23b
ly <lvying@lvchengfuwu.com>
2023-04-04 10:07:33 +0800

    消息页面UI

--- lib/Page/Message/Page/sc_message_page.dart
+++ lib/Page/Message/Page/sc_message_page.dart
@@ -6,6 +6,8 @@ import '../../../../Skin/View/sc_custom_scaffold.dart';
 import '../../../Skin/Tools/sc_scaffold_manager.dart';
 import '../Controller/sc_message_controller.dart';
 import '../View/sc_message_listview.dart';
+import '../View/sc_message_top_dialog.dart';
+import '../View/sc_message_top_item.dart';
 
 /// 消息page
 
@@ -14,7 +16,7 @@ class SCMessagePage extends StatefulWidget {
   SCMessagePageState createState() => SCMessagePageState();
 }
 
-class SCMessagePageState extends State<SCMessagePage> {
+class SCMessagePageState extends State<SCMessagePage> with SingleTickerProviderStateMixin {
 
   /// SCMessageController
   late SCMessageController controller;
@@ -22,11 +24,24 @@ class SCMessagePageState extends State<SCMessagePage> {
   /// SCMessageController - tag
   String controllerTag = '';
 
+
+  late TabController tabController;
+
+  List tabList = ['全部', '未读'];
+
   @override
   initState() {
     super.initState();
+    tabController = TabController(length: tabList.length, vsync: this);
     controllerTag = SCScaffoldManager.instance.getXControllerTag((SCMessagePage).toString());
     controller = Get.put(SCMessageController(), tag: controllerTag);
+    tabController.addListener(() {
+      if (controller.currentIndex != tabController.index) {
+        if (controller.showMoreDialog == true) {
+          controller.updateMoreDialogStatus();
+        }
+      }
+    });
   }
 
   @override
@@ -44,21 +59,60 @@ class SCMessagePageState extends State<SCMessagePage> {
         centerTitle: true,
         navBackgroundColor: SCColors.color_FFFFFF,
         elevation: 0,
-        body: body());
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          color: SCColors.color_F2F3F5,
+          child: body()
+        )
+    );
   }
 
   /// body
   Widget body() {
-    return Container(
-      width: double.infinity,
-      height: double.infinity,
-      color: SCColors.color_F2F3F5,
-      child: GetBuilder<SCMessageController>(
-          tag: controllerTag,
-          init: controller,
-          builder: (state) {
-            return SCMessageListView(state: state);
-          }),
+    return GetBuilder<SCMessageController>(
+      tag: controllerTag,
+      init: controller,
+      builder: (state) {
+        return Stack(
+          alignment: Alignment.topRight,
+          children: [
+            contentItem(),
+            Offstage(
+              offstage: !state.showMoreDialog,
+              child: SCMessageTopDialog(
+                list: const ['全部已读', '全部清除'],
+                tapAction: (index) {
+                  if (index == 0) {
+
+                  } else if (index == 1) {
+
+                  }
+                  controller.updateMoreDialogStatus();
+                },),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget contentItem() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SCMessageTopItem(tabController: tabController, titleList: tabList, moreAction: () {
+          controller.updateMoreDialogStatus();
+        },),
+        Expanded(child: TabBarView(
+            controller: tabController,
+            children: [
+              SCMessageListView(state: controller, type: 0,),
+              SCMessageListView(state: controller, type: 1,),
+            ])
+        ),
+      ],
     );
   }
 }
--- lib/Page/Message/View/sc_message_listview.dart
+++ lib/Page/Message/View/sc_message_listview.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:sc_uikit/sc_uikit.dart';
 import 'package:smartcommunity/Constants/sc_asset.dart';
 import '../Controller/sc_message_controller.dart';
@@ -10,11 +11,29 @@ class SCMessageListView extends StatelessWidget {
   /// SCMessageController
   final SCMessageController state;
 
-  SCMessageListView({Key? key, required this.state}) : super(key: key);
+  /// 类型，0全部，1未读
+  final int type;
+  SCMessageListView({Key? key, required this.state, required this.type}) : super(key: key);
 
+  /// RefreshController
+  RefreshController refreshController = RefreshController(initialRefresh: false);
 
   @override
   Widget build(BuildContext context) {
+    return SmartRefresher(
+        controller: refreshController,
+        enablePullUp: true,
+        enablePullDown: true,
+        header: const SCCustomHeader(
+          style: SCCustomHeaderStyle.noNavigation,
+        ),
+        onRefresh: onRefresh,
+        onLoading: loadMore,
+        child: type == 0 ? listView() : emptyView()
+    );
+  }
+
+  Widget listView() {
     return ListView.separated(
         padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 12.0),
         shrinkWrap: true,
@@ -39,6 +58,50 @@ class SCMessageListView extends StatelessWidget {
           return const SizedBox(height: 10.0,);
         },
         itemCount: 5);
+  }
+
+  /// emptyView
+  Widget emptyView() {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const SizedBox(
+          height: 124.0,
+        ),
+        Image.asset(SCAsset.iconMessageEmpty, width: 120.0, height: 120.0,),
+        const SizedBox(
+          height: 2.0,
+        ),
+        const Text("暂无消息", style: TextStyle(
+            fontSize: SCFonts.f14,
+            fontWeight: FontWeight.w400,
+            color: SCColors.color_8D8E99
+        ),)
+      ],
+    );
+  }
+
+  /// 下拉刷新
+  Future onRefresh() async {
+    state.loadData(
+        isMore: false,
+        completeHandler: (bool success, bool last) {
+          refreshController.refreshCompleted();
+          refreshController.loadComplete();
+        });
+  }
+
+  /// 上拉加载
+  void loadMore() async {
+    state.loadData(
+        isMore: true,
+        completeHandler: (bool success, bool last) {
+          if (last) {
+            refreshController.loadNoData();
+          } else {
+            refreshController.loadComplete();
+          }
+        });
   }
 
 }
